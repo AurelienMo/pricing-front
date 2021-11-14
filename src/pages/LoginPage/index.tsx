715a278218bf4c2d85f9be3e4c1319fa341bd1fa
Aurelien Morvan <morvan.aurelien@gmail.com>
2021-11-14 22:33:47 +0100

    Test : Communcation front with back

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,7 +1,22 @@
 const LoginPage = () => {
+    const debugHandle = async () => {
+        // @ts-ignore
+        const response = await fetch(process.env.REACT_APP_PRICING_API_URL + '/login_check', {
+            method: 'post',
+            body: JSON.stringify({username: 'john@doe.com', password: '123456'}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        if (response.ok) {
+            const data = await response.json();
+            console.warn(data);
+        }
+    }
     return(
         <div>
             Login page
+            <button type="button" onClick={debugHandle}>Debug</button>
         </div>
     )
 }
